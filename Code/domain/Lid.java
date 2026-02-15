d82d4885a324560feaabf8429fb67f093feb9576
SamuelVillagran <samuel.villagranr@gmail.com>
2026-02-14 21:58:08 -0500

    Subiendo el codigo

---
+++ Code/domain/Lid.java
@@ -0,0 +1,48 @@
+package domain;
+import java.util.ArrayList;
+
+
+/**
+ * Write a description of class Lid here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Lid
+{
+    private int idCup;
+    private int height;
+    private int width;
+    private int color;
+    private int xPosition;
+    private int yPosition;
+    private ArrayList<Square> shape;
+
+    /**
+     * Constructor for objects of class Lid
+     */
+    public Lid() {
+    
+    }
+    
+    /**
+     * 
+     */
+    public int getHeight() {
+        return height;
+    }
+    
+    /**
+     * 
+     */
+    public int getWidth() {
+        return width;
+    }
+    
+    /**
+     * 
+     */
+    public void move(int x, int y) {
+        
+    }
+}
