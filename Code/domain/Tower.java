361bfa9b1f6e4264353790a513c92b26e110d831
SamuelVillagran <hsanchezalarcon@gmail.com>
2026-03-03 09:05:38 -0500

    Perfeccionando el método de Stacking

--- Code/domain/Tower.java
+++ Code/domain/Tower.java
@@ -247,6 +247,7 @@ public class Tower {
                 Lid currentLid = lids.get(l);
                 int verifyCup2 = currentCup.getHeight();
                 int verifyLid2 = currentLid.getHeight();
+                counterAdd++;
                 if (verifyLid2 > verifyCup2) {
                     c++;
                     l--;
