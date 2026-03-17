f2e854569dee490991ab298bb6f605cdf5647d11
SamuelVillagran <samuel.villagranr@gmail.com>
2026-03-16 20:23:03 -0500

    Arreglando código de domain

--- Code/domain/Cup.java
+++ Code/domain/Cup.java
@@ -2,7 +2,7 @@ package domain;
 
  
 import java.util.ArrayList;
-import shapes.Rectangle;
+import presentation.Rectangle;
 
 
 /**
--- Code/domain/Lid.java
+++ Code/domain/Lid.java
@@ -2,7 +2,7 @@ package domain;
 
  
 import java.util.ArrayList;
-import shapes.Rectangle;
+import presentation.Rectangle;
 
 /**
  * It represents the lid of the cup
