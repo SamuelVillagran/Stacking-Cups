5b36fbe4dd01fbf73674d4baf050e2831e9f9861
SamuelVillagran <samuel.villagranr@gmail.com>
2026-03-31 15:41:38 -0500

    Perfeccionando pruebas ciclo 4

--- Code/tower/TowerC4Test.java
+++ Code/tower/TowerC4Test.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 
 /**
  * The test class TowerC4Test.
@@ -36,11 +37,23 @@ public class TowerC4Test {
         
         try {
             t.pushCup(6);
+            t.pushCup(5);
+            t.pushCup(4);
+            t.pushLid(4);
+            t.pushLid(5);
             t.pushLid(6);
+            
+            
             t.pushCup("opener", 3); // Should destroy the cup to entry
-            Lid lidOfCup = t.getStackingItems().get(0).getLid();
-            assertEquals(2, t.getStackingItems().size());
-            assertNull(lidOfCup);
+            
+            ArrayList<StackingItem> stackingItems = t.getStackingItems();
+            Lid lidOfCup6 = stackingItems.get(0).getLid(); // Lid of cup with id = 6
+            Lid lidOfCup5 = stackingItems.get(1).getLid(); // Lid of cup with id = 5
+            Lid lidOfCup4 = stackingItems.get(2).getLid(); // Lid of cup with id = 4
+            assertEquals(4, stackingItems.size());
+            assertNull(lidOfCup6);
+            assertNull(lidOfCup5);
+            assertNull(lidOfCup4);
         } 
         catch (tower.TowerException te) {
             te.printStackTrace();
