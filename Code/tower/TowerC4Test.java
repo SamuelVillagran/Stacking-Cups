d5293ce65990aff553aa6cc6c1cabac542833e50
SamuelVillagran <samuel.villagranr@gmail.com>
2026-03-31 11:09:34 -0500

    Agregando clase de excepciones y terminando de insertar el tipo de copa Opener en el método pushCup(type, i)

---
+++ Code/tower/TowerC4Test.java
@@ -0,0 +1,21 @@
+package tower;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class TowerC4Test.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class TowerC4Test {
+    
+    
+    
+    
+}
