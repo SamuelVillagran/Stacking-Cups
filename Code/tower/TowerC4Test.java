26726687e8164eb72f726b353c5cc6b702d4f706
HDavid Sánchez <hsanchezalarcon@gmail.com>
2026-04-01 00:10:17 -0500

    push hierarchichal - simple funcional

--- Code/tower/TowerC4Test.java
+++ Code/tower/TowerC4Test.java
@@ -24,47 +24,38 @@ public class TowerC4Test {
     
     @Test
     public void shouldBeCreateCupOpener() {
-        try {
-            t.pushCup("opener", 3);
+        t.pushCup("opener", 3);
             
-            assertEquals( Opener.class, t.getStackingItems().get(0).getClass());
-        } 
-        catch (tower.TowerException te) {
-            te.printStackTrace();
-        }
+        assertEquals( Opener.class, t.getStackingItems().get(0).getClass());
         
     }
     
     @Test
     public void shouldBePutCorretlyCupOpener() {
-        try {
-            t.pushCup(6);
-            t.pushCup(5);
-            t.pushCup(4);
-            t.pushLid(4);
-            t.pushLid(5);
-            t.pushLid(6);
+        t.pushCup(6);
+        t.pushCup(5);
+        t.pushCup(4);
+        t.pushLid(4);
+        t.pushLid(5);
+        t.pushLid(6);
             
             
-            t.pushCup("opener", 3); // Should destroy the cup to entry
+        t.pushCup("opener", 3); // Should destroy the cup to entry
             
-            ArrayList<StackingItem> stackingItems = t.getStackingItems();
-            Lid lidOfCup6 = stackingItems.get(0).getLid(); // Lid of cup with id = 6
-            Lid lidOfCup5 = stackingItems.get(1).getLid(); // Lid of cup with id = 5
-            Lid lidOfCup4 = stackingItems.get(2).getLid(); // Lid of cup with id = 4
-            assertEquals(4, stackingItems.size());
-            assertNull(lidOfCup6);
-            assertNull(lidOfCup5);
-            assertNull(lidOfCup4);
-        } 
-        catch (tower.TowerException te) {
-            te.printStackTrace();
-        }
+        ArrayList<StackingItem> stackingItems = t.getStackingItems();
+        Lid lidOfCup6 = stackingItems.get(0).getLid(); // Lid of cup with id = 6
+        Lid lidOfCup5 = stackingItems.get(1).getLid(); // Lid of cup with id = 5
+        Lid lidOfCup4 = stackingItems.get(2).getLid(); // Lid of cup with id = 4
+        assertEquals(4, stackingItems.size());
+        assertNull(lidOfCup6);
+        assertNull(lidOfCup5);
+        assertNull(lidOfCup4);
+        
     }
     
     @Test
     public void shouldPushAHierarchichalCup() throws tower.TowerException{
-        t.pushCup("Hierarchical", 6);
+        t.pushCup("hierarchical", 6);
         assertEquals(Hierarchical.class, t.getStackingItems().get(0).getClass());
     }
     
@@ -95,7 +86,7 @@ public class TowerC4Test {
         t.pushCup(1);
         t.pushCup("hierarchical", 5);
             
-        t.swap(new String[]{"cup", "4"}, new String[]{"cup", "1"});
+        t.swap(new String[]{"cup", "5"}, new String[]{"cup", "1"});
             
         String[][] stackingItems = t.stackingItems();
         String[][] expected = {
